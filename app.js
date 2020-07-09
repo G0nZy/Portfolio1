40ae0dfb24512592329f192bc4cc7c251b09bc85
G0nZy <knicksfan1297@gmail.com>
2020-07-09 18:46:45 -0400

    grammar

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Footer from './footer'
+const App = ()=>{
+   return (
+       <div className="App">
+           <h1>Hello React</h1>
+           <Footer/>       </div>
+   )
+}
+
+export default App;
